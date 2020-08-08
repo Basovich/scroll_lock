ae5048ebed11e2ed4806477b19d224db31c2cc4d
Basovich <frghfqv86>
2020-08-09 01:49:01 +0300

    фикс бага с прыжком вверх, если снимаем лок со скрола. обавил проверку, что бы не срабатывало когда нет лок-а на скроле

--- scroll_lock.js
+++ scroll_lock.js
@@ -1,16 +1,24 @@
 (function(){    
     let disableScroll = function disableScroll() {
+        if (document.body.classList.contains('disable-scroll')) {
+            return false;
+        }
         let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
         let pagePosition = window.scrollY;
         document.body.style.paddingRight = paddingOffset;
+        document.body.classList.add('disable-scroll');
         setStyleDisableScroll();
         document.body.dataset.position = pagePosition;
         document.body.style.top = -pagePosition + 'px';
     }
 
     let enableScroll = function enableScroll() {
+        if (!document.body.classList.contains('disable-scroll')) {
+            return false;
+        }
         let pagePosition = parseInt(document.body.dataset.position, 10);
         document.body.style.top = 'auto';
+        document.body.classList.remove('disable-scroll');
         setStyleEnableScroll()
         document.body.style.paddingRight = '';
         window.scroll({ top: pagePosition, left: 0 });
